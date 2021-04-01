f5e5472de86b7c0a59cd21e7d6b88309e317136c
abboudi960 <77051018+abboudi960@users.noreply.github.com>
2021-04-01 16:40:09 +0100

    working_ON_front-end_student_dashboard

--- SCHOOL/student.php
+++ SCHOOL/student.php
@@ -1,3 +1,14 @@
+<?php 
+
+include
+
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,5 +40,61 @@
 
         </header>
     
+                    <h3 class="titles" id="uniqueTitle">Brief</h3>
+
+                <div class="container">
+        
+                <div class="biefList">
+
+                <div class="image_BriefName">
+
+                <i class="fa fa-briefcase" id="iconBrief" aria-hidden="true"></i>
+
+                <h4 class="briefName">brief name</h4>
+                
+                </div>
+
+                </div>
+
+
+                </div>
+
+                <div class="creation">
+
+                <h3 class="titles">Solution Brief</h3>
+
+                    <form action="#" method="post">
+
+                        <input type="text">
+                        <input type="text">
+                        <input type="submit" name="sendSolution" value="send solution">
+
+                    </form>
+
+
+
+
+                </div>
+
+
+
+
+
+        </div>
+
+        <h3 class="titles">Validation list</h3>
+
+        <div class="validation">
+
+                <div>
+                    <img src="" alt="">
+                    <h4 class="briefName">brief name</h4>
+                </div>
+
+
+
+
+        </div>
+
 </body>
 </html>
