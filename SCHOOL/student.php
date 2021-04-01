68c410063b6acf3612edf02fb53e55b2f98b21ae
abboudi960 <77051018+abboudi960@users.noreply.github.com>
2021-04-01 10:10:26 +0100

    deleting_fontAWESOME_file

--- SCHOOL/student.php
+++ SCHOOL/student.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/font-awesome.css">
     <link rel="stylesheet" href="CSS/student.css">
     <title>Document</title>
 </head>
-    <body>
-            <header>
-                    <h2>BFSLearning</h2>
+
+<body>
+
+        <header>
+            <h2>BFSLearning</h2>
                 <div class="navBar">
                     <a href=""></a>
 
@@ -25,7 +26,7 @@
                 </div>
 
 
-                </header>
-        
-    </body>
+        </header>
+    
+</body>
 </html>
