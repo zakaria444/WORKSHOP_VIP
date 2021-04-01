3b6cf535a0739c4f942ad43e1b203711b9a88177
abboudi960 <77051018+abboudi960@users.noreply.github.com>
2021-04-01 11:49:03 +0100

    HTML_CSS_Student_Header

--- SCHOOL/student.php
+++ SCHOOL/student.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/font-awesome.css">
     <link rel="stylesheet" href="CSS/student.css" <?php echo time(); ?>>
     <title>Document</title>
 </head>
@@ -13,14 +14,14 @@
         <header>
             <h2>BFSLearning</h2>
                 <div class="navBar">
-                    <a href=""></a>
+                    <a class="aboutUs" href="">About Us</a>
 
-                    <img src="" alt="">
+                    <i class="fa fa-user-circle-o" aria-hidden="true"></i>
 
                     <div class="username">Username</div>
                     <details>
                            <summary></summary>
-                             <a href="#">Log Out</a>
+                             <a class="logout" href="#">Log Out</a>
                     </details>
                     
                 </div>
